ec79d915427d275980f231a4bc7946da26c09048
Max56132 <50423072+MaxymGorn@users.noreply.github.com>
2020-07-15 20:30:39 +0300

    com134commitcommitingcomittt

---
+++ Presentation Layer/Models/UploadViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Presentation_Layer.Controllers
+{
+    public class UploadViewModel
+    {
+        [Required]
+        public HttpPostedFileBase blob { get; set; }
+        [Required]
+        public string dateStart { get; set; }
+        [Required]
+        public string duration { get; set; }
+    }
+}
